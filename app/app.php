96d72d243b7e0ea3d44a18f2207d3efdcbd30001
zhedgepeth <zhedgepeth1124@gmail.com>
2017-01-01 21:30:18 -0500

    add a view for index store and add store, set routes in app.php to connect to twig views

---
+++ app/app.php
@@ -0,0 +1,39 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->get("/stores/{store_id}", function($store_id) use($app) {
+        $current_store = Store::findById($store_id);
+        return $app['twig']->render('store.html.twig', array(
+            'individual_store' => $current_store,
+            'all_stores' => Store::getAll(),
+            'brands' => $current_store->getBrands()
+        ));
+    });
+
+    return $app;
+
+?>
--- src/Store.php
+++ src/Store.php
@@ -1,5 +1,4 @@
 <?php
-    require_once("src/Brand.php");
 
     class Store
     {
